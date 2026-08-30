ac72a1b16a4eac92528170a29eb79e717e220258
EngReteti <amasonlerionka@gmail.com>
2026-08-30 15:10:06 +0300

    chore: initialize spring boot project skeleton with postgresql config

---
+++ src/main/java/com/amason/hospitalinventory/HospitalInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.amason.hospitalinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amason/hospitalinventory/HospitalInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amason.hospitalinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
